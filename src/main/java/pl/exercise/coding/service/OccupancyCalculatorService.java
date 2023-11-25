6cf0cf2b55c17fb8b1977d2a553b24da2c2e1a1e
Michal Zurek <michal.zurek7@gmail.com>
2023-11-25 12:47:06 +0100

    test1 working - separated proces for eco & premium, set up floating point precision for calc

--- src/main/java/pl/exercise/coding/service/OccupancyCalculatorService.java
+++ src/main/java/pl/exercise/coding/service/OccupancyCalculatorService.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Service;
 import pl.exercise.coding.api.model.CalculateOccupancyPriceRequestDTO;
 import pl.exercise.coding.api.model.CalculateOccupancyPriceResponseDTO;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -14,7 +16,8 @@ public class OccupancyCalculatorService {
 
     public CalculateOccupancyPriceResponseDTO calculateOccupancy(CalculateOccupancyPriceRequestDTO requestDTO) {
 
-        var acc = 0f;
+        var accEco = BigDecimal.ZERO;
+        var accPremium = BigDecimal.ZERO;
         var ecoRooms = requestDTO.getFreeEconomyRooms();
         var premiumRooms = requestDTO.getFreePremiumRooms();
 
@@ -23,19 +26,18 @@ public class OccupancyCalculatorService {
 
         for (Float moneyOffer : toSort) {
             if (premiumRooms > 0) {
-                acc = acc + moneyOffer;
-                premiumRooms = premiumRooms- 1;
-            } else if (ecoRooms > 0) {
-                acc = acc + moneyOffer;
+                accPremium = accPremium.add(new BigDecimal(moneyOffer).setScale(2, RoundingMode.UP));
+                premiumRooms = premiumRooms - 1;
+            } else if (ecoRooms > 0 && moneyOffer < 100) {
+                accEco = accEco.add(new BigDecimal(moneyOffer).setScale(2, RoundingMode.UP));
                 ecoRooms =  ecoRooms - 1;
-            } else {
-                break;
             }
         }
 
 
         CalculateOccupancyPriceResponseDTO calculateOccupancyPriceResponseDTO = new CalculateOccupancyPriceResponseDTO();
-        calculateOccupancyPriceResponseDTO.setPricePremium(acc);
+        calculateOccupancyPriceResponseDTO.setPricePremium(accPremium.floatValue());
+        calculateOccupancyPriceResponseDTO.setPriceEconomy(accEco.floatValue());
         calculateOccupancyPriceResponseDTO.setUsageEconomy(requestDTO.getFreeEconomyRooms() - ecoRooms);
         calculateOccupancyPriceResponseDTO.setUsagePremium(requestDTO.getFreePremiumRooms() - premiumRooms);
         return calculateOccupancyPriceResponseDTO;
